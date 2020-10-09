a75b96002f2fb7da26bf46ea4f2da6e159ae4380
Bjorn Kare Sabo <bjornksa@stud.ntnu.no>
2020-10-09 10:10:10 +0200

    Add loglikelihood

--- gradedIMMPDA/imm.py
+++ gradedIMMPDA/imm.py
@@ -222,12 +222,12 @@ class IMM(Generic[MT]):
         sensor_state: Dict[str, Any] = None,
     ) -> float:
 
-        raise NotImplementedError  # TODO: remove when implemented
-
         mode_conditioned_ll = np.fromiter(
             (
-                None  # TODO: your state filter (fs under) should be able to calculate the mode conditional #log likelihood at z from modestate_s
-                for fs, modestate_s in zip(self.filters, immstate.components) #liste med filter, mixture parameters
+                [
+                    fs.loglikelihood(z, modestate_s, sensor_state=sensor_state)
+                    for fs, modestate_s in zip(self.filters, immstate.components)
+                ]
             ),
             dtype=float,
         )
