b1eba0cfad6f9403e4b5d21aefb849073268b968
Eirik! <eirikjda@ntnu.stud.no>
2020-10-21 10:49:22 +0200

    Kommet til inject

--- gradedINS/eskf.py
+++ gradedINS/eskf.py
@@ -328,7 +328,7 @@ class ESKF:
 
         Ad, GQGd = self.discrete_error_matrices(x_nominal, acceleration, omega, Ts)
 
-        P_predicted = np.zeros((15, 15))
+        P_predicted = Ad@P@Ad.T + GQGd #algorithm 1
 
         assert P_predicted.shape == (
             15,
@@ -380,12 +380,12 @@ class ESKF:
         gyro_bias = self.S_g @ x_nominal[GYRO_BIAS_IDX]
 
         # debias IMU measurements
-        acceleration = np.zeros((3,))
-        omega = np.zeros((3,))
+        acceleration = r_z_acc - acc_bias
+        omega = r_z_gyro - gyro_bias
 
         # perform prediction
-        x_nominal_predicted = np.zeros((16,))
-        P_predicted = np.zeros((15, 15))
+        x_nominal_predicted = self.predict_nominal(x_nominal, acceleration, omega, Ts)
+        P_predicted = self.predict_covariance(x_nominal, P, acceleration, omega, Ts)
 
         assert x_nominal_predicted.shape == (
             16,
