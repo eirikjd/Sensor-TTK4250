8bf3e24bb2cd75618d097c358f42a1506a2ab77e
Bjørn Kåre Sæbø <bjornksa@stud.ntnu.no>
2020-10-26 11:31:22 +0100

    P_pred - > P_est

--- gradedINS/run_INS_simulated.py
+++ gradedINS/run_INS_simulated.py
@@ -192,7 +192,7 @@ doGNSS: bool = True  # TODO: Set this to False if you want to check that the pre
 GNSSk: int = 0  # keep track of current step in GNSS measurements
 for k in tqdm.trange(N):
     if doGNSS and timeIMU[k] >= timeGNSS[GNSSk]:
-        NIS[GNSSk] = eskf.NIS_GNSS_position(x_pred[k], P_est[k], z_GNSS[GNSSk], R_GNSS, lever_arm)
+        NIS[GNSSk] = eskf.NIS_GNSS_position(x_pred[k], P_pred[k], z_GNSS[GNSSk], R_GNSS, lever_arm)
         # eskf.innovation_GNSS_position(x_pred[k], P_pred[k], z_GNSS[GNSSk], R_GNSS, lever_arm)
 
         x_est[k], P_est[k] = eskf.update_GNSS_position(x_pred[k], P_pred[k], z_GNSS[GNSSk], R_GNSS, lever_arm)
